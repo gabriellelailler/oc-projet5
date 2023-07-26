4ad07001ce2f32b48d200f09dff68ea3e3668eba
gabriellelailler <gabrielle.lailler@gmail.com>
2023-07-26 09:54:02 +0200

    clean bootsrap

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,16 +1,36 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
-});
+// Import the mauGallery function from the mauGallery.js file
+import { mauGallery } from '/mauGallery.js';
 
+// Define the gallery configuration
+const galleryConfig = {
+    columns: {
+        xs: 1,
+        sm: 2,
+        md: 3,
+        lg: 3,
+        xl: 3
+    },
+    lightBox: true,
+    lightboxId: 'myAwesomeLightbox',
+    showTags: true,
+    tagsPosition: 'top'
+};
+
+// Check if ES modules are supported in the browser
+const isModuleSupported = 'noModule' in document.createElement('script');
+
+// Function to initialize the gallery
+function initializeGallery() {
+    // Use the mauGallery function to initialize the gallery
+    mauGallery(galleryConfig);
+}
+
+// Run the gallery initialization function when the document is ready
+document.addEventListener('DOMContentLoaded', () => {
+    // Call the appropriate initialization function based on browser support
+    if (isModuleSupported) {
+        initializeGallery(); // For modern browsers
+    } else {
+        mauGallery(galleryConfig); // For legacy browsers
+    }
+});
